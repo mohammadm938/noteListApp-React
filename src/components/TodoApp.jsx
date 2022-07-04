b427f3eeaeb9f552bb2eedaf71777391a659491c
mohammadm938 <imMohammadm938@gmail.com>
2022-07-04 15:24:56 +0430

    add flter todos

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "./Navbar";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
+  const [status, setStatus] = useState("All");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    filterTodos(status);
+  }, [todos, status]);
 
   const addTodoHandler = (input) => {
     console.log(input);
@@ -38,6 +44,25 @@ const TodoApp = () => {
     setTodos(clonedState);
   };
 
+  const filterTodos = (status) => {
+    switch (status) {
+      case "Complete":
+        setFilteredTodos(todos.filter((t) => t.isCompleted));
+        break;
+      case "Uncomplete":
+        setFilteredTodos(todos.filter((t) => !t.isCompleted));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  const selectHandler = (e) => {
+    console.log(e.target.value);
+    setStatus(e.target.value);
+  };
+
   return (
     <div className="flex flex-col items-center bg-slate-700 h-screen p-4 text-white">
       <NavBar
@@ -47,11 +72,13 @@ const TodoApp = () => {
         <TodoForm addTodoHandler={addTodoHandler} />
       ) : null}
       <TodoList
-        todos={todos}
+        todos={filteredTodos}
         onCompleteHandler={completeHandler}
         onDeleteHandler={deleteHandler}
         onUpdateTodo={updateTodo}
         setIsEditingState={setIsEditing}
+        status={status}
+        onSelectHandler={selectHandler}
       />
     </div>
   );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -8,6 +8,8 @@ const TodoList = ({
   onDeleteHandler,
   onUpdateTodo,
   setIsEditingState,
+  status,
+  onSelectHandler,
 }) => {
   const [edit, setEdit] = useState({ id: null, text: "", isCompleted: false });
 
@@ -20,6 +22,15 @@ const TodoList = ({
   const renderTodos = () => {
     return (
       <div className="flex flex-col justify-center items-center space-y-4 bg-slate-600 p-4 w-3/4 max-w-3xl  shadow-xl rounded-md mt-8">
+        <select
+          className="p-2 bg-slate-800 rounded-md"
+          onChange={onSelectHandler}
+          value={status}
+        >
+          <option value="All">All</option>
+          <option value="Complete">Complete</option>
+          <option value="Uncomplete">Uncomplete</option>
+        </select>
         {todos.length === 0 ? (
           <p className="bg-slate-700 p-2 w-1/2 flex justify-center items-center font-bold drop-shadow-md">
             Add some todos
